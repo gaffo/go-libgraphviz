1eb8b8570fb96b289b8f27c3c58263903bf1a1d6
Mike Gaffney <mr.gaffo@gmail.com>
2017-02-14 00:04:48 -0800

    Moar mapped

--- graphviz.go
+++ graphviz.go
@@ -7,12 +7,15 @@ import ()
 // #include "gvc.h"
 import "C"
 
+var AGDIRECTED = C.Agdirected
+
 type Graphviz struct {
 	gv *C.GVC_t
 }
 
 func NewGraphviz() *Graphviz {
 	gv := C.gvContext()
+	C.gvParseArgs(gv, 0, nil)
 	return &Graphviz{
 		gv: gv,
 	}
@@ -20,4 +23,53 @@ func NewGraphviz() *Graphviz {
 
 func (this *Graphviz) Close() {
 	C.gvFreeContext(this.gv)
+}
+
+func (this *Graphviz) LayoutJobs(graph *Graph) {
+	C.gvLayoutJobs(this.gv, graph.g)
+}
+
+func (this *Graphviz) RenderJobs(graph *Graph) {
+	C.gvRenderJobs(this.gv, graph.g)
+}
+
+func (this *Graphviz) FreeLayout(graph *Graph) {
+	C.gvFreeLayout(this.gv, graph.g)
+}
+
+type Graph struct {
+	g *C.Agraph_t
+}
+
+func NewGraph(text string, desc C.Agdesc_t) *Graph {
+	g := C.agopen(C.CString(text), desc, nil)
+	return &Graph{
+		g: g,
+	}
+}
+
+func (this *Graph) Close() {
+	C.agclose(this.g)
+}
+
+type Node struct {
+	n *C.Agnode_t
+}
+
+func NewNode(graph *Graph, text string, create int) *Node {
+	n := C.agnode(graph.g, C.CString(text), C.int(create))
+	return &Node{
+		n: n,
+	}
+}
+
+type Edge struct {
+	e *C.Agedge_t
+}
+
+func NewEdge(graph *Graph, node1, node2 *Node, text string, create int) *Edge {
+	e := C.agedge(graph.g, node1.n, node2.n, C.CString(text), C.int(create))
+	return &Edge{
+		e: e,
+	}
 }
--- gvtest/main.go
+++ gvtest/main.go
@@ -10,4 +10,17 @@ func main() {
 	defer gv.Close()
 
 	fmt.Println("GV Initted")
+
+	g := graphviz.NewGraph("g", graphviz.AGDIRECTED)
+	n := graphviz.NewNode(g, "n", 1)
+	m := graphviz.NewNode(g, "m", 1)
+	graphviz.NewEdge(g, n, m, "label", 1)
+
+	gv.LayoutJobs(g)
+
+	gv.RenderJobs(g)
+
+	gv.FreeLayout(g)
+
+	g.Close()
 }
